7826496a0c141a5d2f8ef6ad602a3fc9c71a42ec
mercury <udtrokia@gmail.com>
2018-08-18 23:18:56 +0800

    [TEST]: TEST PACKAGE

---
+++ src/test/word_test.go
@@ -0,0 +1,19 @@
+package word
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`);
+	}
+
+	if !IsPalindrome("merem") {
+		t.Error(`IsPalindrome("Mercury") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome") {
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
